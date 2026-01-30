eefb7968ec176ee177addd165065035defd65ef2
fbagaglia <genspark_dev@genspark.ai>
2026-01-30 00:30:12 +0000

    Set Vercel runtime nodejs

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,7 @@ import { handle } from '@hono/node-server/vercel'
 import app from '../hono-api/src/index'
 
 export const config = {
-  runtime: 'nodejs20.x',
+  runtime: 'nodejs',
 }
 
 export default handle(app)
