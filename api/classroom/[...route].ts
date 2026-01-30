2cf6f13438c09f5b093579314be5fe712681fe56
fbagaglia <genspark_dev@genspark.ai>
2026-01-30 10:51:41 +0000

    Scope classroom api route for Vercel

--- api/classroom/[...route].ts
+++ api/classroom/[...route].ts
@@ -1,5 +1,5 @@
 import { handle } from '@hono/node-server/vercel'
-import app from '../hono-api/src/index'
+import app from '../../hono-api/src/index'
 
 export const config = {
   runtime: 'nodejs',
